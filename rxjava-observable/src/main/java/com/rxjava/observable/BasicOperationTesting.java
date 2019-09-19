707b356b0fab38db3424f355694150a90979ea9f
crossborderkey <dszz112@163.com>
2019-09-19 14:02:22 +0800

    测试flatMap

--- rxjava-observable/src/main/java/com/rxjava/observable/BasicOperationTesting.java
+++ rxjava-observable/src/main/java/com/rxjava/observable/BasicOperationTesting.java
@@ -2,8 +2,29 @@ package com.rxjava.observable;
 
 import io.reactivex.Observable;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class BasicOperationTesting {
     public static void main(String[] args) {
+//        filterAndMap();
+
+        flatMapTesting();
+    }
+
+    static void flatMapTesting() {
+        //flatMap大多的应用场景是根据子元素再做一些额外的操作 - rxjava示例
+        Observable.fromArray(1, 2, 3).flatMap(x -> Observable.range(x, 3)).subscribe(System.out::println);
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+        //Java8的Lambda表达式
+        Arrays.asList(1, 2, 3).stream().flatMap(item -> Stream.of(item + "-admin")).forEach(System.out::println);
+
+    }
+
+
+    static void filterAndMap() {
         //Observable的Filter过滤操作
         Observable.range(1, 10)
                 .filter(i -> i % 2 == 0)
@@ -19,6 +40,5 @@ public class BasicOperationTesting {
         Observable.fromArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
                 .map(x -> x + "-from-array")
                 .subscribe(System.out::println);
-
     }
 }
