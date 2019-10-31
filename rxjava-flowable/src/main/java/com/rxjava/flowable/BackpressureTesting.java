e5915328658f821a0653ce4f6854c9492aecfd97
crossborderkey <dszz112@163.com>
2019-10-31 13:42:12 +0800

    Flowable的简单使用V1.0

--- rxjava-flowable/src/main/java/com/rxjava/flowable/BackpressureTesting.java
+++ rxjava-flowable/src/main/java/com/rxjava/flowable/BackpressureTesting.java
@@ -10,6 +10,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class BackpressureTesting {
     public static void main(String[] args) throws InterruptedException {
+
+        //设置buffer的缓存大小，默认128
+//        System.setProperty("rx2.buffer-size","998");
+
+
+//        simpleBackpressure();
+
+
+
+
+
+        Thread.sleep(100000);
+    }
+
+    static void simpleBackpressure(){
         Flowable<Object> objectFlowable = Flowable.create(new FlowableOnSubscribe<Object>() {
             @Override
             public void subscribe(FlowableEmitter<Object> emitter) throws Exception {
@@ -22,10 +37,9 @@ public class BackpressureTesting {
         objectFlowable.doOnNext(e -> System.out.println("元素数：" + e))
                 .observeOn(Schedulers.computation())
                 .subscribe(element -> {
-                    Thread.sleep(100);
+                    Thread.sleep(20);
                     System.out.println("元素消费：" + element);
                 });
 
-        Thread.sleep(100000);
     }
 }
